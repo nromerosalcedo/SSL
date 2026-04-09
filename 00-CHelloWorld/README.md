136b61f8159f6e2044f701ceb87a6b0e3a8bbc7a
nromerosalcedo <nromerosalcedo@frba.utn.edu.ar>
2026-04-09 20:19:56 -0300

    Primer ejercicio completo!

--- 00-CHelloWorld/README.md
+++ 00-CHelloWorld/README.md
@@ -1,3 +1,5 @@
-# 00 - C Hello World
-
+# 00 - C Hello World    
 Primer programa en C.
+
+Compilador: GCC
+Version 14.2.0
---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (){
+    printf("Hello, World!!!\n");
+    printf("Version de C: %ld\n", __STDC_VERSION__);
+    return 0;
+}
